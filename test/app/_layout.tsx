cb3201a4e3dd045a0858c9ca43b4774e4b089a79
dimonkrutoiprogrammist <dmitry2008cita@gmail.com>
2026-03-09 19:08:39 +0900

    Add files via upload

---
+++ test/app/_layout.tsx
@@ -0,0 +1,16 @@
+// Импорт Stack из библиотеки 'expo-router'
+import { Stack } from 'expo-router';
+
+// Экспорт RootLayout
+export default function RootLayout() {
+  return (
+    // Создаем контейнер Stack для управления навигацией
+    <Stack>
+      {/* 
+        name="index": указывает, что это экран соответствует файлу index.tsx
+        options={{ title: "Todo" }}: задает заголовок экрана "Todo"
+      */}
+      <Stack.Screen name="index" options={{ title: "Todo" }} />
+    </Stack>
+  );
+}
